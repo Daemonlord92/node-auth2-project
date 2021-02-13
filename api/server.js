81226d44c29b12cb84dae1309ab1ab36742c8adc
Matthew Martin <zhintateo19924559@gmail.com>
2021-02-13 10:50:27 -0600

    Created the server file with error checking middle ware

---
+++ api/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get("/", (req, res) => {
+	res.json({
+		api: "up"
+	});
+});
+
+// MIDDLEWARE
+
+server.use((err, req, res, next) => {
+	err.statusCode = err.statusCode ? err.statusCode : 500;
+	res.status(err.statusCode).json({
+		mes: err.message,
+		stack: err.stack
+	})
+})
+
+
+module.exports = server;
